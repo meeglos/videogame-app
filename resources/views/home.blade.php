6a48a208a470a7d614cfca087d880a4a832d5e13
Miguel Rodríguez <miguel@codde.it>
2020-05-29 02:49:47 +0200

    initial layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,207 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center">
-        <div class="md:w-1/2 md:mx-auto">
-
-            @if (session('status'))
-                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-
-                <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
-                    Dashboard
-                </div>
-
-                <div class="w-full p-6">
-                    <p class="text-gray-700">
-                        You are logged in!
-                    </p>
+<div class="container mx-auto">
+    <h2 class="text-blue-500 uppercase tracking-wide font-semibold">popular games</h2>
+    {{-- <div class="popular-games text-sm grid-cols-6 gap-12 border-b border-gray-800 pb-16"> --}}
+    <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b
+    border-gray-800 pb-16">
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/avengers-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
                 </div>
             </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Sonic
+                Forces United Enemies</a>
+            <div class="text-gray-400 mt-1">Nintendo Switch</div>
         </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/residentevil-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        80%
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy VII
+                Remake</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/spiderman-cover.png" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Sonic
+                Forces United Enemies</a>
+            <div class="text-gray-400 mt-1">Nintendo Switch</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/sonicforces-cover.png" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        80%
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy VII
+                Remake</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/supermarioparty-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Sonic
+                Forces United Enemies</a>
+            <div class="text-gray-400 mt-1">Nintendo Switch</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/sonicracing-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        80%
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy VII
+                Remake</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="./img/uncharted4-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Uncharted 4</a>
+            <div class="text-gray-400 mt-1">Nintendo Switch</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/fifa19-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        80%
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Fifa 19</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="./img/covid-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Uncharted 4</a>
+            <div class="text-gray-400 mt-1">Nintendo Switch</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/animalcrossing-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        80%
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Fifa 19</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="./img/thecomplex-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Uncharted 4</a>
+            <div class="text-gray-400 mt-1">Nintendo Switch</div>
+        </div>
+
+        <div class="game mt-8">
+            <div class="relative inline-block">
+                <a href="#">
+                    <img src="img/starwars-cover.jpg" alt="game cover"
+                        class="hover:opacity-75 transition ease-in-out duration-150 h-64">
+                </a>
+                <div
+                    class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full transform translate-x-8 translate-y-8">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        80%
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Fifa 19</a>
+            <div class="text-gray-400 mt-1">Playstation 4</div>
+        </div>
+
     </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,43 +12,75 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100 h-screen antialiased leading-none">
+
+<body class="bg-gray-900 h-screen antialiased leading-none text-white">
     <div id="app">
-        <nav class="bg-blue-900 shadow mb-8 py-6">
-            <div class="container mx-auto px-6 md:px-0">
-                <div class="flex items-center justify-center">
-                    <div class="mr-6">
+        <nav class="mb-8 px-4 py-6 border-b border-gray-800">
+            <div class="container mx-auto px-6 md:px-0 flex items-center justify-between">
+                <div class="left-menu flex items-center">
+                    <div class="mr-8">
                         <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
                             {{ config('app.name', 'Laravel') }}
                         </a>
                     </div>
-                    <div class="flex-1 text-right">
+                    <ul class="flex text-gray-600 space-x-4 small-caps">
+                        <li><a href="#" class="hover:text-gray-300">games</a></li>
+                        <li><a href="#" class="hover:text-gray-300">reviews</a></li>
+                        <li><a href="#" class="hover:text-gray-300">offers</a></li>
+                        <li><a href="#" class="hover:text-gray-300">coming soon</a></li>
+                    </ul>
+                </div>
+                <div class="right-menu flex items-center">
+                    <div class="search relative mr-4">
+                        <input type="text" name="search"
+                            class="bg-gray-800 text-sm rounded-full px-3 py-1 pl-8 focus:outline-none focus:shadow-outline text-gray-400"
+                            placeholder="Search">
+                        <div class="absolute top-0 flex items-center h-full ml-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff"
+                                class="fill-current text-gray-400 w-4">
+                                <path d="M0 0h24v24H0z" fill="none" />
+                                <path
+                                    d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="flex items-center">
                         @guest
-                            <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            @if (Route::has('register'))
-                                <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            @endif
+                        <a class="no-underline hover:underline text-gray-300 text-sm p-3"
+                            href="{{ route('login') }}">{{ __('Login') }}</a>
+                        @if (Route::has('register'))
+                        <a class="no-underline hover:underline text-gray-300 text-sm p-3"
+                            href="{{ route('register') }}">{{ __('Register') }}</a>
+                        @endif
                         @else
-                            <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
+                        <span class="text-gray-300 text-sm pr-2">{{ Auth::user()->name }}</span>
 
+                        <section>
                             <a href="{{ route('logout') }}"
-                               class="no-underline hover:underline text-gray-300 text-sm p-3"
-                               onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                                class="no-underline hover:underline text-gray-300 text-sm p-3" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                                 {{ csrf_field() }}
                             </form>
+                        </section>
                         @endguest
                     </div>
                 </div>
             </div>
         </nav>
 
-        @yield('content')
+        <main class="py-8">@yield('content')</main>
+
+        <footer class="border-t border-gray-800 text-gray-100">
+            <div class="container mx-auto py-6">
+                Powered by <a href="#" class="underline hover:text-gray-500">IGDB API</a>
+            </div>
+        </footer>
     </div>
 </body>
+
 </html>
